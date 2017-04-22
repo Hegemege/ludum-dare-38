746fb27b2200b528e13a517f9d324121e85a48d5
Henri Niva <henri.niva@aalto.fi>
2017-04-22 13:57:58 +0300

    Sphere model + smoothing

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,9 +4,11 @@ using System.Collections;
 public class CameraController : MonoBehaviour
 {
     public GameObject FollowTarget;
+    public GameObject PlanetReference;
 
     public float TargetDistance;
     public float TargetHeight;
+    public float TargetAngle;
 
     void Awake()
     {
@@ -26,5 +28,19 @@ public class CameraController : MonoBehaviour
     void FixedUpdate()
     {
         float dt = Time.fixedDeltaTime;
+
+        Vector3 towardsPlanet = PlanetReference.transform.position - transform.position;
+
+        // Make the camera follow the target at thte given distance and height
+        // Place us behind the player at the given distance
+
+        transform.position = FollowTarget.transform.position - FollowTarget.transform.forward * TargetDistance;
+        transform.position += -towardsPlanet.normalized * TargetHeight;
+
+        // TODO: Add smoothing
+        // Apply look rotation
+        var towardsTarget = FollowTarget.transform.position - transform.position;
+        var localUp = Quaternion.AngleAxis(-TargetAngle, FollowTarget.transform.right);
+        transform.rotation = localUp * Quaternion.LookRotation(towardsTarget, -towardsPlanet);
     }
 }
---
+++ Assets/Scripts/SurfaceAlign.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SurfaceAlign : MonoBehaviour
+{
+    public GameObject PlanetReference;
+
+    void Awake() 
+    {
+
+    }
+
+    void Start() 
+    {
+        // Project current forward onto the plane defined by the surface normal
+        var normal = (transform.position - PlanetReference.transform.position).normalized;
+        var projectedForward = Vector3.ProjectOnPlane(transform.forward, normal);
+
+        transform.rotation = Quaternion.LookRotation(projectedForward, normal);
+    }
+    
+    void Update() 
+    {
+    
+    }
+}
