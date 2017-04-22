7bb48f7260d7df4c23fd59fd69f6c905db864cb2
Henri Niva <henri.niva@aalto.fi>
2017-04-22 12:59:06 +0300

    Plane + controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject FollowTarget;
+
+    public float TargetDistance;
+    public float TargetHeight;
+
+    void Awake()
+    {
+
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        float dt = Time.fixedDeltaTime;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Security.Cryptography;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject PlanetReference;
+
+    // TMEP
+    public GameObject debugSphere;
+
+    // Physics parameters
+    public float Airspeed;
+    public float TurningSpeed;
+
+    public float DistanceFromGround;
+
+    public LayerMask CollidableLayerMask;
+    public LayerMask GroundLayerMask;
+
+    // Physics helpers
+    [HideInInspector]
+    public Vector3 Velocity;
+
+    // Privates
+    private CharacterController characterController;
+
+    // Input
+    private float HorizontalInput
+    {
+        get
+        {
+            return Input.GetAxis("Horizontal");
+        }
+    }
+
+    private float VerticalInput
+    {
+        get
+        {
+            return Input.GetAxis("Vertical");
+        }
+    }
+
+    void Awake() 
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Start()
+    {
+        // Initialize velocity as forward
+        Velocity = Airspeed * new Vector3(0f, 0f, 1f);
+
+        // Move the player to be at the correct height
+        var groundPoint = GroundPosition(transform.position);
+        var planetToGround = (groundPoint - PlanetReference.transform.position).normalized;
+        transform.position = groundPoint + planetToGround * DistanceFromGround;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float dt = Time.fixedDeltaTime;
+        // Helper vector towards the planet from the player
+        Vector3 towardsPlanet = PlanetReference.transform.position - transform.position;
+
+        // Input
+        var newForward = Velocity.normalized;
+
+        // Turning
+        newForward = Quaternion.AngleAxis(HorizontalInput * TurningSpeed * dt, -towardsPlanet) * newForward;
+
+        Velocity += Airspeed * newForward;
+
+        // Project velocity to be at constant distance from ground
+        Vector3 groundPoint = GroundPosition(transform.position + Velocity * dt);
+        var planetToGround = (groundPoint - PlanetReference.transform.position).normalized;
+
+        var normalizedTarget = groundPoint + planetToGround * DistanceFromGround;
+        var normalizedVelocity = normalizedTarget - transform.position;
+
+        debugSphere.transform.position = groundPoint;
+
+        // Apply projected velocity
+        Velocity = normalizedVelocity;
+
+        characterController.Move(Velocity * dt);
+
+        // Rotate the player to travel forward
+        transform.rotation = Quaternion.LookRotation(Velocity, -towardsPlanet);
+    }
+
+    /// <summary>
+    /// Returns the distance to ground at current location
+    /// </summary>
+    private Vector3 GroundPosition(Vector3 from)
+    {
+        Vector3 towardsPlanet = PlanetReference.transform.position - from;
+        Ray ray = new Ray(from, towardsPlanet);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, GroundLayerMask))
+        {
+            return hit.point;
+        }
+
+        Debug.LogWarning("No ground under player. Shouldn't happen.");
+        return Vector3.zero;
+    }
+}
