c786ec584f61b9c089b5cf7ca0dad68be9edb037
Henri Niva <henri.niva@aalto.fi>
2017-04-24 02:21:27 +0300

    UI Tweaks

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,6 +1,5 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEditor;
 using UnityEngine.SceneManagement;
 
 public class GameState : MonoBehaviour 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
-using UnityEditor;
 
 public class PlayerController : MonoBehaviour
 {
